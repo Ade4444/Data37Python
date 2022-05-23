d52def106c91da7ae217fdc1b187b72c0db178d6
ade akinyemi <adeakinyemi@hotmail.co.uk>
2022-05-23 07:57:43 +0100

    add the rest of my PY files

---
+++ variables/sets Dicts.py
@@ -0,0 +1,22 @@
+mcu = {"Iron Man", "Thor", "Captain America", "Hulk"}
+
+print(mcu, type(mcu))
+
+mcu.add("Hawkeye")
+mcu.add("Black Widow")
+
+print(mcu)
+
+print(mcu.pop())
+print(mcu.pop())
+print(mcu.pop())
+
+mcu.discard("Iron man")
+
+print(mcu)
+
+nums = [1, 3, 5, 7, 9]
+print(nums)
+
+print(list(set(nums)))
+
---
+++ variables/tuples.py
@@ -0,0 +1,15 @@
+anime = ("Dragon Ball Z", "One Piece", "Ai", "Hunter X Hunter")
+
+print(anime)
+
+print(anime, type(anime))
+
+print(anime[-1])
+
+# anime[0] = "Yu-Gi-Oh" tuples are immutable, cannot have assignments, cannot remove from tuples either
+
+print(anime.count("One Piece"))
+
+print(anime.index("Hunter X Hunter"))
+
+
